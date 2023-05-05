58c413bd8026389d81e205a20f04e0928063d627
Court-Room-Gaming <111709947+Court-Room-Gaming@users.noreply.github.com>
2023-05-05 02:43:40 -0700

    Create vehicles-lua.bat

---
+++ vehicles-lua.bat
@@ -0,0 +1,24 @@
+@echo off
+setlocal EnableDelayedExpansion
+
+set LUA_FILE=output.lua
+
+echo "return {" > %LUA_FILE%
+
+for %%f in (*.ytd) do (
+    set name=%%~nf
+    set name=!name:_= !
+    echo "['!name!'] = {" >> %LUA_FILE%
+    echo "    ['name'] = '!name!', " >> %LUA_FILE%
+    echo "    ['model'] = '!name!', " >> %LUA_FILE%
+    echo "    ['price'] = 20000, " >> %LUA_FILE%
+    echo "    ['category'] = 'compacts', " >> %LUA_FILE%
+    echo "    ['categoryLabel'] = 'Compacts', " >> %LUA_FILE%
+    echo "    ['hash'] = `!name!`, " >> %LUA_FILE%
+    echo "    ['shop'] = 'pdm', " >> %LUA_FILE%
+    echo "}, " >> %LUA_FILE%
+)
+
+echo "}" >> %LUA_FILE%
+
+echo "Done."
