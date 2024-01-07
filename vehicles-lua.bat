5d8dd55c4f0bcc81722112b5cbd84f45f1a83369
Court-Room-Gaming <111709947+Court-Room-Gaming@users.noreply.github.com>
2024-01-06 23:35:53 -0700

    Update vehicles-lua.bat
    Updated For the new QB-core Format

--- vehicles-lua.bat
+++ vehicles-lua.bat
@@ -8,15 +8,7 @@ echo "return {" > %LUA_FILE%
 for %%f in (*.ytd) do (
     set name=%%~nf
     set name=!name:_= !
-    echo "['!name!'] = {" >> %LUA_FILE%
-    echo "    ['name'] = '!name!', " >> %LUA_FILE%
-    echo "    ['model'] = '!name!', " >> %LUA_FILE%
-    echo "    ['price'] = 20000, " >> %LUA_FILE%
-    echo "    ['category'] = 'compacts', " >> %LUA_FILE%
-    echo "    ['categoryLabel'] = 'Compacts', " >> %LUA_FILE%
-    echo "    ['hash'] = '!name!', " >> %LUA_FILE%
-    echo "    ['shop'] = 'pdm', " >> %LUA_FILE%
-    echo "}, " >> %LUA_FILE%
+ echo "    { model = '!name!', name = '!name!', brand = 'somebrand', price = 125000, category = 'compacts', type = 'automobile', shop = 'pdm' }," >> %LUA_FILE%
 )
 
 echo "}" >> %LUA_FILE%
